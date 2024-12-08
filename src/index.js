7a6700c5945236f669dfe29cebfe9fdcabe9c166
Vivek Sharma <viveksharma2382000@gmail.com>
2024-12-08 16:47:11 +0530

    feat: utility classes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import dotenv from "dotenv";
 import connectDB from "./db/db.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path: "./env"
-})
+  path: "./env",
+});
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is up and running at ${process.env.PORT}`)
+    })
+  })
+  .catch((err) => {
+    console.error("MONGODB connection failed: ", err);
+  });
