34994f6950fdf6950b2ccb7347db7882a0e1c964
Vivek Sharma <viveksharma2382000@gmail.com>
2024-12-10 02:00:09 +0530

    fix: dotenv import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-require("dotenv").config();
-
+import dotenv from "dotenv";
 import connectDB from "./db/db.js";
 import { app } from "./app.js";
+
+dotenv.config({
+  path: "./env",
+});
 
 connectDB()
   .then(() => {
